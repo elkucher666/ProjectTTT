6c02e05971d6bec94b1b5f3de8e0c0c841620ab1
elkucher666 <mr.boss.shu@mail.ru>
2024-06-08 13:02:52 +0300

    Chenge name game

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bot = new TelegramBot(TOKEN, {
     polling: true
 });
 const port = process.env.PORT || 5000;
-const gameName = "FirstClicker";
+const gameName = "TTT-TheThroneTON";
 const queries = {};
 server.use(express.static(path.join(__dirname, 'ProjectTTT')));
 bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "Say /game if you want to play."));
